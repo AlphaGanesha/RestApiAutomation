fb985552d650154bd8641d08c7cd6167dadbad67
MeFromOtherDevice <ganeshias3198@gmail.com>
2025-09-16 11:03:19 +0530

    cleaned code according to SonarCube suggestions and added {userName} to update and delete routs

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -9,6 +9,6 @@ public class Routes {
 
     public static String create_user_url = base_url+"/user";
     public static String get_user_url = base_url+"/user/{username}";
-    public static String update_user_url = base_url+"/user/{username}";
-    public static String delete_user_url = base_url+"/user/{username}";
+    public static String update_user_url = base_url+"/user/";
+    public static String delete_user_url = base_url+"/user/";
 }
