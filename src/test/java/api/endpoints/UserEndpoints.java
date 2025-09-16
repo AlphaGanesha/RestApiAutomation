dd86b2498e7cd2598c8b320519967b498c96f9c4
MeFromOtherDevice <ganeshias3198@gmail.com>
2025-09-16 11:02:00 +0530

    cleaned code according to SonarCube suggestions and added {userName} to update and delete routs

--- src/test/java/api/endpoints/UserEndpoints.java
+++ src/test/java/api/endpoints/UserEndpoints.java
@@ -1,47 +1,43 @@
 package api.endpoints;
 
 import static io.restassured.RestAssured.given;
+
+import api.payload.User;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class UserEndpoints {
        public static Response createUser(User payload){
-                Response response = given()
+           return given()
                         .contentType(ContentType.JSON)
                         .accept(ContentType.JSON)
                         .body(payload)
                         .when()
                         .post(Routes.create_user_url);
-
-                return response;
         }
 
     public static Response readUser(String userName){
-        Response response = given()
+
+        return given()
                 .pathParam("username",userName)
                 .when()
                 .get(Routes.get_user_url);
-
-        return response;
     }
 
     public static Response updateUser(String userName, User payload){
-        Response response = given()
+        return given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(payload)
                 .when()
-                .put(Routes.update_user_url);
-
-        return response;
+                .put(Routes.update_user_url+userName);
     }
 
     public static Response deleteUser(String userName){
-        Response response = given()
+
+        return given()
                 .pathParam("username", userName)
                 .when()
-                .delete(Routes.delete_user_url);
-
-        return response;
+                .delete(Routes.delete_user_url+userName);
     }
 }
